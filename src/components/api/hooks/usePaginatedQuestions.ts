f87e11d6e75fe67829998c215a428f416ddb5901
rakaprdana <raka.pradana1103@gmail.com>
2025-06-28 11:10:37 +0700

    fix: api question(not yet)

--- src/components/api/hooks/usePaginatedQuestions.ts
+++ src/components/api/hooks/usePaginatedQuestions.ts
@@ -6,40 +6,40 @@ import { useEffect, useState } from "react";
 import type { Question } from "../../../interfaces/interface";
 import { getQuestion } from "../api";
 
-// export function usePaginatedQuestions() {
-//   const [questions, setQuestion] = useState<Question[]>([]);
-//   const [currentPage, setCurrentPage] = useState(1);
-//   const [totalPages, setTotalPages] = useState(0);
-//   useEffect(() => {
-//     const fetchData = async () => {
-//       try {
-//         const response = await getQuestion(currentPage);
-//         setQuestion(response.questions);
-//         setTotalPages(response.totalPages);
-//       } catch (error) {
-//         console.error("Error fetching data: ", error);
-//       }
-//     };
-//     fetchData();
-//   }, [currentPage]);
+/*export function usePaginatedQuestions() {
+  const [questions, setQuestion] = useState<Question[]>([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await getQuestion(currentPage);
+        setQuestion(response.questions);
+        setTotalPages(response.totalPages);
+      } catch (error) {
+        console.error("Error fetching data: ", error);
+      }
+    };
+    fetchData();
+  }, [currentPage]);
 
-//   const nextPage = () => {
-//     if (currentPage < totalPages) setCurrentPage(currentPage + 1);
-//   };
+  const nextPage = () => {
+    if (currentPage < totalPages) setCurrentPage(currentPage + 1);
+  };
 
-//   const prevPage = () => {
-//     if (currentPage > 1) setCurrentPage(currentPage - 1);
-//   };
+  const prevPage = () => {
+    if (currentPage > 1) setCurrentPage(currentPage - 1);
+  };
 
-//   return {
-//     questions,
-//     currentPage,
-//     totalPages,
-//     nextPage,
-//     prevPage,
-//     setCurrentPage,
-//   };
-// }
+  return {
+    questions,
+    currentPage,
+    totalPages,
+    nextPage,
+    prevPage,
+    setCurrentPage,
+  };
+}*/
 
 export function GetQuestion() {
   const [question, setQuestion] = useState<Question[]>([]);
--- src/components/pages/QuestionPage.tsx
+++ src/components/pages/QuestionPage.tsx
@@ -1,6 +1,5 @@
-import { useEffect, useState } from "react";
 import type { Question, Answer } from "../../interfaces/interface";
-import { getQuestion } from "../api/api";
+import { GetQuestion } from "../api/hooks/usePaginatedQuestions";
 
 interface Props {
   questions: Question[];
@@ -8,13 +7,7 @@ interface Props {
   onAnswer: (questionId: number, confidence: number) => void;
 }
 
-export const QuestionPage: React.FC<Props> = ({
-  questions,
-  answers,
-  onAnswer,
-}) => {
-  const [question, setQuestion] = useState<Question[]>([]);
-
+export const QuestionPage: React.FC<Props> = ({ answers, onAnswer }) => {
   const choices = [
     { label: "Tidak Tahu", value: 0 },
     { label: "Tidak Yakin", value: 0.2 },
@@ -23,62 +16,56 @@ export const QuestionPage: React.FC<Props> = ({
     { label: "Cukup Yakin", value: 0.8 },
     { label: "Sangat Yakin", value: 1 },
   ];
-  useEffect(() => {
-    async function fetchData() {
-      try {
-        setQuestion(await getQuestion());
-      } catch (error) {
-        console.error("Error: ", error);
-      }
-    }
-    fetchData();
-  }, []);
-
+  const question = GetQuestion();
   return (
     <main className="flex flex-col items-center space-y-6 px-4 md:px-8">
-      {question.map((q) => {
-        const currentAnswer = answers.find((a) => a.questionId === q.id);
-
-        return (
-          <div
-            key={q.id}
-            className="flex flex-col md:flex-row md:flex-wrap items-start md:items-center justify-between gap-4 p-6 border border-borderColor rounded-2xl bg-foreBackground w-full max-w-5xl shadow-sm"
-          >
-            {/* Pertanyaan */}
-            <p className=" text-base md:text-lg font-medium text-textBold w-full md:w-1/3 min-w-0 break-words">
-              {q.text}
-            </p>
-
-            {/* Pilihan Jawaban */}
-            <div className="grid grid-cols-2 sm:grid-cols-3 gap-3 sm:gap-4 w-full md:w-[60%]">
-              {choices.map((choice) => {
-                const selected = currentAnswer?.confidence === choice.value;
-                return (
-                  <label
-                    key={choice.value}
-                    className={` flex items-center justify-center  cursor-pointer rounded-xl border px-2 py-2 text-center font-medium transition w-full
+      {question.length === 0 ? (
+        <p className="text-center text-textMedium text-lg">
+          Tidak ada pertanyaan
+        </p>
+      ) : (
+        question.map((q) => {
+          const currentAnswer = answers.find((a) => a.questionId === q.id);
+          return (
+            <div
+              key={q.id}
+              className="flex flex-col md:flex-row md:flex-wrap items-start md:items-center justify-between gap-4 p-6 border border-borderColor rounded-2xl bg-foreBackground w-full max-w-5xl shadow-sm"
+            >
+              {/* Pertanyaan */}
+              <p className=" text-base md:text-lg font-medium text-textBold w-full md:w-1/3 min-w-0 break-words">
+                {q.text}
+              </p>
+              {/* Pilihan Jawaban */}
+              <div className="grid grid-cols-2 sm:grid-cols-3 gap-3 sm:gap-4 w-full md:w-[60%]">
+                {choices.map((choice) => {
+                  const selected = currentAnswer?.confidence === choice.value;
+                  return (
+                    <label
+                      key={choice.value}
+                      className={` flex items-center justify-center  cursor-pointer rounded-xl border px-2 py-2 text-center font-medium transition w-full
                 ${
                   selected
                     ? "bg-selected text-white border-selected"
                     : "bg-white text-textMedium border-borderColor hover:bg-background"
                 }`}
-                  >
-                    <input
-                      type="radio"
-                      name={`q-${q.id}`}
-                      value={choice.value}
-                      className="hidden "
-                      checked={selected}
-                      onChange={() => onAnswer(q.id, choice.value)}
-                    />
-                    {choice.label}
-                  </label>
-                );
-              })}
+                    >
+                      <input
+                        type="radio"
+                        name={`q-${q.id}`}
+                        value={choice.value}
+                        className="hidden "
+                        checked={selected}
+                        onChange={() => onAnswer(q.id, choice.value)}
+                      />
+                      {choice.label}
+                    </label>
+                  );
+                })}
+              </div>
             </div>
-          </div>
-        );
-      })}
+          );
+        })
+      )}
     </main>
   );
 };
